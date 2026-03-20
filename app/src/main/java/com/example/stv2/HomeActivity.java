cfe5e7b8a0dd73086757930aec558dc622219b92
Radics Krisztina <h376543@stud.u-szeged.hu>
2026-03-20 16:07:26 +0100

    nightmode off

--- app/src/main/java/com/example/stv2/HomeActivity.java
+++ app/src/main/java/com/example/stv2/HomeActivity.java
@@ -2,9 +2,12 @@ package com.example.stv2;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatDelegate;
+
 public class HomeActivity extends MenuActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open);
 
